24104acdecf1442c268b19bb76d5819c9d5964d8
nicolassyuki <108940147+nicolassyuki@users.noreply.github.com>
2022-10-07 11:06:56 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,20 +1,44 @@
-let tabuada = 12;
+let tabuada = 22;
+let nome = "Rodrigo";
 
 function escreva(){
-    document.write("tabuada do " + tabuada + "<br>")
-    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
-    document.write("Feito por " + Nicolas);
+    document.write("Tabuada do " + tabuada + "<br>")
+     document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
+     document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
+     document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
+     document.write(tabuada+" x 4 = "+(tabuada*4)+"<br>");
+     document.write(tabuada+" x 5 = "+(tabuada*5)+"<br>");
+     document.write(tabuada+" x 6 = "+(tabuada*6)+"<br>");
+     document.write(tabuada+" x 7 = "+(tabuada*7)+"<br>");
+     document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
+     document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
+     document.write(tabuada+" x 10 = "+(tabuada*10)+"<br><br>");
+     document.write("Feito por " + Rodrigo);
+
 }
 
+ function minhaTabuada(){
+     for(let i = 1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j<= 10; j++){
+            document.write(i+ " x "+ j + " = "+(j*i)+"<br>");
+        }
+         document.write("<br>");
 
-    
+         
+     }
+ }
+
+ function quadrado(){
+     for(let i = 2; i <= 20; i++){
+         document.write("O quadrado de " + i + " é " + (i*i)+ "<br>")
+     }
+ }
+ 
+ function calcula(){
+     let c = document.getElementById("Valor").value;
+     let j = document.getElementById("Juros").value;
+     let r = c * (1 + (j/100));
+     document.write("Resultado:  " + r);
+ }
 
